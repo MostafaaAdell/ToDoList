8f9d3af9ec6f5d1f04e11d14eb911f5b3cf65f2c
MustafaaAdell <Mustafaadell9777@gmail.com>
2023-08-30 10:14:04 +0300

    Initial Commit

---
+++ ToDoList/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ToDoList
+//
+//  Created by Mustafa on 30/08/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ ToDoList/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Mustafa on 30/08/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
